8c215b68261a34090bc6e67e6faad5a2a9fe1ada
Angel-Ort3ga <alconblack11@gmail.com>
2024-09-15 21:22:33 -0700

    Mapa creado

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,20 @@
-import TileMap from "./tileMap.js";
+import TileMap from "./TileMap.js";
 
-const tileSize = 32
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+const tileSize = 32;
+const canvas = document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
 const tileMap = new TileMap(tileSize);
 
-function gameLoop(){
-    tileMap.draw();
-
-}  
+// Configura el tamaño del canvas basado en el mapa
 tileMap.setCanvasSize(canvas);
 
-setInterval(gameLoop,1000/75);
+function gameLoop() {
+  // Limpiar el canvas antes de dibujar
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Llama al método draw con el contexto del canvas
+  tileMap.draw(ctx);
+}
+
+// Llama a gameLoop a una tasa de 75 FPS
+setInterval(gameLoop, 1000 / 75);
--- src/tileMap.js
+++ src/tileMap.js
@@ -1,72 +1,81 @@
-export default class tileMap{
-    constructor(){
-        this.tileSize = this.tileSize;
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+    this.imagesLoaded = false;
 
-        this.muro = new Image()
-        this.muro.src = "../images/muro.png"
+    this.muro = new Image();
+    this.muro.src = "../images/muro.png";
+    this.muro.onload = () => this.checkImagesLoaded();
 
-        this.ball1 = new Image()
-        this.ball1.src = "../images/ball1.png"
+    this.ball1 = new Image();
+    this.ball1.src = "../images/ball1.png";
+    this.ball1.onload = () => this.checkImagesLoaded();
+  }
 
-    }  
+  checkImagesLoaded() {
+    // Verificar  imágenes
+    if (this.muro.complete && this.ball1.complete) {
+      this.imagesLoaded = true;
+    }
+  }
 
-    
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
 
-    map = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]
+  draw(ctx) {
+    if (!this.imagesLoaded) {
+      console.log("Imágenes aún no cargadas");
+      return;
+    }
 
-   /* draw(ctx){
-        for(let row=0; row < this.map.length; re++){
-            for(let colum=0; colum < this.map[row].length; colum++){
-                let tile = this.map[row][colum];
-                if(tile === 1){
-                    this.#drawWall(ctx, colum, row, this,this.tileSize);
-                }
-                else if (tile === 0){
-                    this.#drawDot(ctx, colum, row, this,this.tileSize);
-                }
-            }
+    for (let row = 0; row < this.map.length; row++) {
+      for (let colum = 0; colum < this.map[row].length; colum++) {
+        let tile = this.map[row][colum];
+        if (tile === 1) {
+          this.drawWall(ctx, colum, row, this.tileSize);
+        } else if (tile === 0) {
+          this.drawDot(ctx, colum, row, this.tileSize);
         }
-
+      }
     }
+  }
 
-    #drawDot(ctx,colum,row,size){
-        ctx.drawImage(
-            this.ball1,
-            colum * this.tileSize,
-            row * this.tileSize,
-            size,
-            size
-        )
-    }
+  drawDot(ctx, colum, row, size) {
+    ctx.drawImage(
+      this.ball1,
+      colum * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
 
-    #drawWall(ctx,colum,row,size) {
-        ctx.drawImage(
-            this.muro,
-            colum * this.tileSize,
-            row * this.tileSize,
-            size,
-            size
-        );
-    }*/
-     
-    setCanvasSize(canvas){
-        canvas.width = this.map[0].length * this.tileSize;
-        canvas.height = this.map.length * this.tileSize;
-    }
+  drawWall(ctx, colum, row, size) {
+    ctx.drawImage(
+      this.muro,
+      colum * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
 }
